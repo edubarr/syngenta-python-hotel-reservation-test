4102ae7f40aa1fccde368832ccac103fae4c90c1
edubarr <eduaraujobarros@gmail.com>
2022-05-12 10:23:18 -0300

    added more tests and comments

--- src/my_module.py
+++ src/my_module.py
@@ -1,4 +1,7 @@
 def get_hotel_info(hotel_name):
+    '''Takes a string with the hotel name, and returns all the info about the hotel (rating and price rates).'''
+
+    #Dict of hotel infos
     hotel_info = {
         "Lakewood": {
             "rating": 3,
@@ -16,14 +19,16 @@ def get_hotel_info(hotel_name):
             "reward": {"week": 100, "weekend": 40},
         },
     }
-
     return hotel_info[hotel_name]
 
 def get_cheapest_quotes(week_days, client_type):
+    '''Takes a list of week (and weekend) days and the type of the client and return a list of cheapest hotels (one or more)'''
+
     hotels = ["Lakewood", "Bridgewood", "Ridgewood"]
 
     hotels_quotes = {}
 
+    # Loop through all hotels for every day of the list and saves the total quote on the hotels_quotes dict
     for hotel in hotels:
         hotels_quotes[hotel] = 0
         hotel_rates = get_hotel_info(hotel)
@@ -33,12 +38,15 @@ def get_cheapest_quotes(week_days, client_type):
             else:
                 hotels_quotes[hotel] += hotel_rates[client_type]["week"]
     
+    # Chooses the cheapest hotels on the dict and return a list
     cheapest_quote = min(hotels_quotes.values())
     cheapest_hotels = [hotel for hotel in hotels_quotes.keys() if hotels_quotes[hotel] == cheapest_quote]
 
     return cheapest_hotels
 
 def check_quotes(cheapest_quotes):
+    '''Check the list of cheapest hotels and return the best of them based on the rating'''
+
     hotel_ratings = [get_hotel_info(hotel)["rating"] for hotel in cheapest_quotes]
     
     best_hotel = cheapest_quotes[hotel_ratings.index(max(hotel_ratings))]
@@ -46,17 +54,22 @@ def check_quotes(cheapest_quotes):
     return best_hotel
 
 def get_cheapest_hotel(number):  # DO NOT change the function's name
+    # Split the input into the type of client and days of reservation
     input = number.split(sep = ":")
 
+    # Takes the type of client and convert to needed format
     client_type = input[0].lower()
     if client_type == "rewards":
         client_type = "reward"
 
+    # Split the string that contains all dates into a list of dates and takes only the week day part into another list
     dates = input[1].split(sep = ",")
     week_days = [date[11:-1] for date in dates]
 
+    # Get cheapest quotes from the function
     cheapest_quotes = get_cheapest_quotes(week_days, client_type)
     
+    # Check if have more than one cheapest quote, and get best hotel
     if len(cheapest_quotes) > 1:
         cheapest_hotel = check_quotes(cheapest_quotes)
     else:
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -3,32 +3,58 @@ from context import src
 from src.my_module import get_cheapest_hotel
 
 class MyTest(TestCase):
-    # Regular, 3 weekdays OK
+    # test samples
     def test1(self):
         result = "Lakewood"
         self.assertEqual(result, get_cheapest_hotel("Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)"))
 
-    # Regular, 2 weekdays and 1 weekend day
     def test2(self):
-        result = "Lakewood"
-        self.assertEqual(result, get_cheapest_hotel("Regular: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)"))
-
-    # Regular, 1 weekday and 2 weekend day
-    def test3(self):
         result = "Bridgewood"
         self.assertEqual(result, get_cheapest_hotel("Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)"))
 
-    # Rewards, 3 weekdays
+    def test3(self):
+        result = "Ridgewood"
+        self.assertEqual(result, get_cheapest_hotel("Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)"))
+    
+
+    #Implemented tests:
+
+    # Regular, all week days
     def test4(self):
+        result = "Lakewood"
+        self.assertEqual(result, get_cheapest_hotel("Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed), 19Mar2009(thur)"))
+
+    # Regular, more weekdays than weekend days
+    def test5(self):
+        result = "Lakewood"
+        self.assertEqual(result, get_cheapest_hotel("Regular: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)"))
+
+    # Regular, less weekday than weekend day
+    def test6(self):
+        result = "Bridgewood"
+        self.assertEqual(result, get_cheapest_hotel("Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)"))
+
+    # Regular, all weekend days
+    def test7(self):
+        result = "Bridgewood"
+        self.assertEqual(result, get_cheapest_hotel("Regular: 21Mar2009(sat), 22Mar2009(sun)"))
+
+    # Rewards, all week days
+    def test8(self):
         result = "Lakewood"
         self.assertEqual(result, get_cheapest_hotel("Rewards: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)"))
 
-    # Rewards, 2 weekdays and 1 weekend day
-    def test5(self):
+    # Rewards, more weekdays than weekend days
+    def test9(self):
         result = "Ridgewood"
         self.assertEqual(result, get_cheapest_hotel("Rewards: 26Mar2009(thur), 27Mar2009(fri), 28Mar2009(sat)"))
 
-    # Rewards, 1 weekday and 2 weekend day
-    def test6(self):
+    # Rewards, less weekday than weekend day
+    def test10(self):
         result = "Ridgewood"
         self.assertEqual(result, get_cheapest_hotel("Rewards: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)"))
+
+    # Rewards, all weekend days
+    def test11(self):
+        result = "Ridgewood"
+        self.assertEqual(result, get_cheapest_hotel("Rewards: 21Mar2009(sat), 22Mar2009(sun)"))
